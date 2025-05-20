5020e93519bd9aab6fab9d6539410683930bdde0
Antience100 <190290455+Antience100@users.noreply.github.com>
2025-05-20 18:58:01 -0400

    finished functionality of calculator, going to touch it up with some more styling, and add keyboard functionality

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -72,44 +72,47 @@ function divide(a, b) {
   }
 }
 
-// Handles click -- work in progress --
+// Handles click -- finished?
+// Runs through the click options when clicked
 function handleClick(click) {
+  // clears all the calculation data saved for operations
   if (click === "clear") {
     clearCalculations();
+  // Calculates and returns the result to the display using whichever operater and numbers has been inputted
   } else if (click === "=") {
     if (numbers.length > 0 && operators.length > 0) {
       numbers.push(displayValue);
-      displayValue = operate().toString();
-      clearCalculations();
+      displayValue = operate();
+      numbers = [];
+      operators = [];
+      hasDecimal = false;
       updateDisplay();
     }
+  // uses handleSign function to change the displayValue from either negative or positive
   } else if (click === "sign") {
     handleSign();
+  // uses handlePercent function to simply divide by 100 and give the percentage of the displayValue
   } else if (click === "percent") {
     handlePercent();
+  // Whatever number between 0 - 9 that has been clicked will be added to the displayValue
   } else if (click >= "0" && click <= "9") {
     appendToDisplay(click);
+  // Will add a decimal to the displayValue as long as there isn't one already
   } else if (click === ".") {
     if (!hasDecimal) {
-        hasDecimal = true;
-        appendToDisplay(click);
+      hasDecimal = true;
+      appendToDisplay(click);
     }
+  // Goes through the click options for operators and adds the correct operator to be used in the operate function, 
+  // adds the current displayValue to numbers, 
+  // and clears the display for the next value to be started
   } else if (["+", "-", "*", "/"].includes(click)) {
     if (displayValue !== "0" || numbers.length > 0) {
-        numbers.push(displayValue);
-        operators.push(click);
-        if (numbers.length > 1) {
-            displayValue = operate();
-            numbers = [displayValue];
-            operators = [click];
-            updateDisplay();
-        }
-        clearDisplay();
-    } else {
-        operators.push(click);
+      numbers.push(displayValue);
+      operators.push(click);
+      clearDisplay(0);
     }
   }
-  console.log("displayValue: " + displayValue + " numbers: " + numbers);
 }
 
 // Sets up eventListeners on ever button on the document with a click event, using the event handler handleClick to use the value of the button clicked to decide what to do from there
@@ -125,7 +128,7 @@ function setUpEventListeners() {
 updateDisplay();
 setUpEventListeners();
 
-// Operation function to use a sign to determine which calculation to complete
+// Operation function to use a sign to determine which calculation to complete and returns the answer
 function operate() {
   if (numbers.length < 1 || operators.length < 1) return displayValue;
 
@@ -138,10 +141,11 @@ function operate() {
       result = subtract(result, nextNum);
     } else if (operators[i] === "*") {
       result = multiply(result, nextNum);
-    } else if (operate[i] === "/") {
+    } else if (operators[i] === "/") {
       result = divide(result, nextNum);
       if (result === "Error") return result;
     }
   }
+  console.log("result: " + result.toString());
   return result.toString();
 }
